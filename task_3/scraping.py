017ae4efcbc2289205dbd96c418c4a13cbd32492
giicoo <arnst.artyom@gmail.com>
2025-06-05 09:39:11 +0300

    task_3 dry fixs

--- task_3/scraping.py
+++ task_3/scraping.py
@@ -2,7 +2,6 @@ import re
 import requests
 from bs4 import BeautifulSoup
 import time
-import numpy as np
 import pandas as pd
 
 dt = pd.DataFrame(columns=["title", "year", "rating","votes","runtime","genres"])
@@ -13,19 +12,23 @@ response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.content, 'html.parser')
 time.sleep(1)
 
-matches = re.findall(r'https://www\.imdb\.com/title/tt\d+/', soup.prettify())
+'''
+почему-то в Dev Tools отображалось сразу все 250 фильмов, но как оказалось они подгружались из js во время прокрутку
+поэтому нашел список ссылок внутри js и спарсил оттуда
+'''
+matches = re.findall(r'https://www\.imdb\.com/title/tt\d+/', soup.prettify()) 
 for i in matches:
     try:
         response = requests.get(i, headers=headers)
         soup = BeautifulSoup(response.content, 'html.parser')
-        #TODO: DRY
+        class_any = soup.find(class_="ipc-inline-list ipc-inline-list--show-dividers sc-103e4e3c-2 cMcwpt baseAlt baseAlt").find_all("li")
+        d["runtime"]+=[class_any[-1].text]
+        d["year"]+=[class_any[0].text]
+
         d["title"]+=[soup.find(class_="hero__primary-text").text]
-        d["year"]+=[soup.find(class_="ipc-inline-list ipc-inline-list--show-dividers sc-103e4e3c-2 cMcwpt baseAlt baseAlt").find_all("li")[0].text]
         d["rating"]+=[soup.find(class_="sc-d541859f-1 imUuxf").text]
         d["votes"]+=[soup.find(class_="sc-d541859f-3 dwhNqC").text]
-        d["runtime"]+=[soup.find(class_="ipc-inline-list ipc-inline-list--show-dividers sc-103e4e3c-2 cMcwpt baseAlt baseAlt").find_all("li")[-1].text]
         d["genres"]+=[soup.find(class_="ipc-chip ipc-chip--on-baseAlt").text]
-        print(pd.DataFrame(d))
         time.sleep(1)
     except Exception as e:
         print(e)
